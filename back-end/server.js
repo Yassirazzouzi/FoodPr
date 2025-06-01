c5e39a0e0b927e554bd4347e054512bed03aabe3
Yassir Azzouzi <yassirazzouzi6@gmail.com>
2025-06-01 14:53:51 +0100

    ttw1

--- back-end/server.js
+++ back-end/server.js
@@ -21,7 +21,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 // Connect to MongoDB
 connectDB();
