f8a427bde3521f00342c294bffd11f8bffd11da5
Yassir Azzouzi <yassirazzouzi6@gmail.com>
2025-06-01 16:20:48 +0100

    yy

--- back-end/server.js
+++ back-end/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://your-frontend-url.vercel.app'],
+  origin: [],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
